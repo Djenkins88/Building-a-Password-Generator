5171ead0ddc4a43afd61af9c3899e47ca788bc4b
Dominque Jenkins <dominquejenkins14@gmail.com>
2021-05-28 13:56:54 -0500

    added pwInpt to input

--- script.js
+++ script.js
@@ -1,68 +1,130 @@
-// Assignment Code
- var generateBtn = document.querySelector("#generate");
- 
+// Assignmen
+var lowerArr = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
 
- 
- // Write password to the #password input
+var upperArr = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
+var numberArr = numbers[("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")];
+
+var specialArr = [
+  "@",
+  "%",
+  "+",
+  "/",
+  "'",
+  "!",
+  "#",
+  "$",
+  "^",
+  "?",
+  ":",
+  ",",
+  ")",
+  "(",
+  "}",
+  "{",
+  "]",
+  "[",
+  "~",
+  "-",
+  "_",
+  ".",
+];
+
+// Write password to the #password input
 function writePassword() {
-  //charsets is a var for character to be choosen from.
-  var charsets = 'abcdefghijklmnopqrstuvwxqzABCDEFGHIJKLMNOPRSTUVWXYZ123456789!@#$%^&';
-  // setting size to charsets so the entire string can be accessed 
-  var  size = charsets.length;
-  var password = generatePassword();  
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-  // running a for loop so charsets characters can be loop through until done 
-  for (i = 0; i < charsets.length; i++) {
-    generatePassword = generatePassword + charsets.length(Math.floor(Math.random() * size));
-  
-  }
-  password();  
-  
-  //var password = generatePassword();  
-  //var passwordText = document.querySelector("#password");
+  // // running a for loop so charsets characters can be loop through until done
+  // //for (i = 0; i < charsets; i++) {
+  //   //password = password + charsets(Math.floor(Math.random() * size));
 
-
-  //passwordText.value = password;
-
+  // }
 }
 
-
-
+var generateBtn = document.querySelector("#generate");
 //Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+alert("criteria for password");
+
+function getPwInput() {
+  // pronpt for questions about password
+
+  var length = parseInt(
+    prompt("Password length has to be 8 to 128 characters):", "enter tag")
+  );
+
+  var upper = confirm("Do you want upper cases in your password?");
+  var lower = confirm("Do you want lower cases in your password?");
+  var numbers = confirm("Do you want numbers in your password?");
+  var specialCharacters = confirm(
+    "Do you want special characters in your password?"
+  );
+
+  var pwInput = {
+    length: length,
+    hasUpper: upper,
+    hasLower: lower,
+    hasNum: numbers,
+    hasSpecs: specialCharacters,
 
 
-// pronpt for questions about password
-var criteria = prompt("Password citeria includes length, upper, lower, numeric, and special characters.");
-var length = prompt("Password length has to be 8 to 128 characters):","enter tag");
-var upper = prompt("Do you want upper cases in your password?")
-var lower = prompt("Do you want lower cases in your password?")
-var numbers = prompt("Do you want numbers in your password?")
-var specialCharacters = prompt("Do you want special characters in your password?")
-
-
-/*ar legth = "";
-var numbers = "";
-var lower = "";
-var upper = "";
-var specialCharacters = "";
-
-for (i = 0; i < length; i++) {
-    length+=charsets(Math.floor(Math.random() *charsets.length));
-    numbers+=charsets(Math.floor(Math.random() *charsers.length));
-    lower+=charsets(Math.floor(Math.random() * charsets));
-    upper+=charsets(Math.floor(Math.random() * charsets));
-    specialCharacters+=charsets(Math.floor(Math.random() * charsets));*/
-
-
-
-
-
-
-
-
-
-
+  };
+  return pwInput
+}
 
